6ab11860090806a45627875b3bae062553aab32b
Lucas Consejo <lucas.consejo@ynov.com>
2021-10-29 13:01:26 +0200

    ✨ update splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import useCachedResources from './src/hooks/useCachedResources';
 import Navigation from './src/navigation';
 
-const App: React.FC = () => (
-  <Navigation />
-);
+const App: React.FC = () => {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } 
+  return <Navigation />
+};
 
 export default App;
---
+++ src/hooks/useCachedResources.ts
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
+
+const useCachedResources = () => {
+    const [isLoadingComplete, setIsLoadingComplete] = useState(false)
+
+    useEffect(() => {
+        const loadDataAndResourcesAsync = async () => {
+            try {
+                SplashScreen.preventAutoHideAsync()
+                await Font.loadAsync({
+                    UberMoveMedium: require('../assets/fonts/UberMoveMedium.otf'),
+                    UberMoveBold: require('../assets/fonts/UberMoveBold.otf'),
+                });
+                await new Promise(resolve => setTimeout(resolve, 2000));
+            } catch (e) {
+                console.warn(e)
+            } finally {
+                SplashScreen.hideAsync()
+                setIsLoadingComplete(true)
+            }
+        }
+
+        loadDataAndResourcesAsync()
+    }, [])
+
+    return isLoadingComplete
+}
+
+export default useCachedResources
+ 
