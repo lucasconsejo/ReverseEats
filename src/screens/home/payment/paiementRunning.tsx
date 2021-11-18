c51b1ea7c6e2c1441e229ee479394a3b0e21cd8b
Tonyto <pedrero.tony@yahoo.com>
2021-11-18 14:48:29 +0100

    ✨ Update PaymentRunning screen

--- src/screens/home/payment/paiementRunning.tsx
+++ src/screens/home/payment/paiementRunning.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Logo from '../../../assets/light-logo.svg'
+import { colors } from '../../../theme/colors'
 
 const paiementRunning: React.FC = () => {
+
+    useEffect( () => {
+        setTimeout(()=>{
+            console.log("waited");
+       }, 5000);
+    }, [])
+
     return (
-        <View>
-            <Text>Paiement en cours</Text>
+        <View style={{ flex: 1, backgroundColor: colors.white }}>
+            <View style={{ flex: 6, alignItems: 'center', justifyContent: 'center'}}>
+                <Logo/>
+                <Text style={{ fontSize: 22, marginTop: 10 }}>Transaction en cours...</Text>
+
+            </View>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{ fontSize: 21, fontFamily: "UberMoveMedium" }}>Ne quittez pas l'application</Text>
+            </View>
         </View>
     )
 }
