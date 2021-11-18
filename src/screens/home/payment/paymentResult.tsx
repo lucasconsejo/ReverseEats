bb2e8f62b8e97c81aac74c40bba83c454177dd44
Lucas Consejo <lucas.consejo@ynov.com>
2021-11-18 15:57:37 +0100

    🐛 fix check icon

--- src/screens/home/payment/paymentResult.tsx
+++ src/screens/home/payment/paymentResult.tsx
@@ -1,45 +1,42 @@
 import React from 'react'
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { ScreenProps } from '../../../types/props.types'
 import PaymentValid from '../../../assets/paymentValid.svg'
 import Patrick from '../../../assets/sponge-bob-patrick-star.gif'
 import Button from '../../../theme/buttons'
 import { colors } from '../../../theme/colors'
 
-const PaymentResult: React.FC<ScreenProps> = ({ navigation }) => {
-
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
-            <View style={{ flex: 3, marginHorizontal: 25, justifyContent: "space-between" }}>
-                <View style={{ flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
-                    <Text style={{ fontSize: 26, fontFamily: "UberMoveBold" }}>Merci pour votre commande</Text>
-                    <PaymentValid/>
-                </View>
-                <View style={{ flex: 1 }}>
-                    <Text style={{ fontSize: 20, fontFamily: "UberMoveMedium"}}>Paiement effectué avec succès</Text>
-                    <Text style={{ fontSize: 18, marginTop: 15}}>Vous allez être notifié quand le cuisinier se déplacera vers le lieu de rendez-vous.</Text>
-                </View>
+const PaymentResult: React.FC<ScreenProps> = ({ navigation }) => (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
+        <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+        <View style={{ flex: 3, marginHorizontal: 25, justifyContent: "space-between" }}>
+            <View style={{ flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+                <Text style={{ fontSize: 26, fontFamily: "UberMoveBold", width: "80%" }}>Merci pour votre commande</Text>
+                <PaymentValid/>
             </View>
-            <View style={{flex:3, marginHorizontal: 25}}>
-                <Text style={{ fontSize: 20, fontFamily: "UberMoveMedium", marginBottom: 15}}>Bon appétit !</Text>
-                <Image style={{ width:330, height: 210, alignSelf: "center"}} source={Patrick}/>
+            <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 20, fontFamily: "UberMoveMedium"}}>Paiement effectué avec succès</Text>
+                <Text style={{ fontSize: 18, marginTop: 15}}>Vous allez être notifié quand le cuisinier se déplacera vers le lieu de rendez-vous.</Text>
             </View>
-            <View style={{ flex: 1, marginHorizontal: 25}}>
-                <Button 
-                    theme="secondaryDarkRight" 
-                    style={{ marginTop: 35, backgroundColor: colors.white}} 
-                    title="Retour à l'accueil" 
-                    onPress={ () => navigation.reset({
-                        index: 0,
-                            routes: [{ name: "Home" }],
-                        })}
-                    active={true}
-                />
-            </View>
-        </SafeAreaView>
-    )
-}
+        </View>
+        <View style={{flex:3, marginHorizontal: 25}}>
+            <Text style={{ fontSize: 20, fontFamily: "UberMoveMedium", marginBottom: 15}}>Bon appétit !</Text>
+            <Image style={{ width:330, height: 210, alignSelf: "center"}} source={Patrick}/>
+        </View>
+        <View style={{ flex: 1, marginHorizontal: 25}}>
+            <Button
+                theme="secondaryDarkRight"
+                style={{ marginTop: 35, backgroundColor: colors.white}}
+                title="Retour à l'accueil"
+                onPress={ () => navigation.reset({
+                    index: 0,
+                        routes: [{ name: "Home" }],
+                    })}
+                active={true}
+            />
+        </View>
+    </SafeAreaView>
+)
 
 export default PaymentResult
 
-const styles = StyleSheet.create({})
